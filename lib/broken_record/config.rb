a11229505ab8b261981ae4989553139f7e8d80bb
Brendan Dougherty <b.w.dougherty@gmail.com>
2014-01-30 13:37:01 -0500

    Initialize BrokenRecord::Config.before_scan_callbacks
    * Prevents nil reference in BrokenRecord::Scanner
      when no callbacks have been defined

--- lib/broken_record/config.rb
+++ lib/broken_record/config.rb
@@ -2,9 +2,9 @@ module BrokenRecord
   module Config
     extend self
     attr_accessor :classes_to_skip, :before_scan_callbacks
+    self.before_scan_callbacks = []
 
     def before_scan(&block)
-      self.before_scan_callbacks ||= []
       self.before_scan_callbacks << block
     end
   end
